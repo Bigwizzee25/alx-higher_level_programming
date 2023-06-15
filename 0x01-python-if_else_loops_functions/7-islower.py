0072e915a20c6139666eaf95bf8a2b1d224dced6
Bigwizzee25 <wisdomnkobie@gmail.com>
2023-06-15 08:57:01 +0100

    final commit

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+
+def islower(c):
+    Argsc = 'c'
+    ascii_code = ord(c)
+    for i in range(97, 123):
+        if islower(c):
+            print("a is{}".format("lower" if islower("a") else "upper"))
+        
